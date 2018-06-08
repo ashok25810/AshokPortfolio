a5b666ddf4ce757389f7ddf1f6b83e9bd1ae0b58
ashok25810 <ashok25810@users.noreply.github.com>
2018-06-08 15:27:55 -0500

    Destroy feature added

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,11 +14,11 @@ def create
 
     respond_to do |format|
       if @portfolio_items.save
-        format.html { redirect_to @portfolio_path, notice: 'Portfolio was successfully created.' }
-        format.json { render :show, status: :created, location: @portfolio_items }
+        format.html { redirect_to @portfolios_url, notice: 'Portfolio was successfully created.' }
+       # format.json { render :show, status: :created, location: @portfolio_items }
       else
         format.html { render :new }
-        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+       # format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
       end
     end
 	
@@ -35,11 +35,11 @@ def update
 
 	respond_to do |format|
       if @portfolio_items.save
-        format.html { redirect_to @portfolio_path, notice: 'Portfolio was successfully created.' }
-        format.json { render :show, status: :created, location: @portfolio_items }
+        format.html { redirect_to @portfolios_path, notice: 'Portfolio was successfully updated.' }
+        
       else
         format.html { render :edit }
-        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+        
       end
     end
 end
