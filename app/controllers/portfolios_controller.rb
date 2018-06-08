e17f8f902ea98c4fed1112584c21a39f07869be8
ashok25810 <ashok25810@users.noreply.github.com>
2018-06-08 11:53:28 -0500

    With the edit, update, add, show functionalities

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,51 @@
 class PortfoliosController < ApplicationController
+
 def index
 	@portfolio_items=Portfolio.all
+end
+
+def new 
+	@portfolio_items=Portfolio.new
+end
+
+
+def create
+	 @portfolio_items = Portfolio.new( params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to @portfolio_path, notice: 'Portfolio was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio_items }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+      end
+    end
+	
+end
+
+def edit
+	@portfolio_items=Portfolio.find(params[:id])
+end
+
+
+def update
+	
+	@portfolio_items=Portfolio.find(params[:id])
+
+	respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to @portfolio_path, notice: 'Portfolio was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio_items }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+      end
+    end
+end
+
+def show 
+	@portfolio_items=Portfolio.find(params[:id])
 end
 
 end
